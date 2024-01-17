ab663d7997dfc488b1e2f5cbb30610bc6e29142b
Ayush Kaithwas <ayush.91011@gmail.com>
2024-01-18 04:20:16 +0530

    logging

--- internal/models/transcode.go
+++ internal/models/transcode.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"strconv"
 	"strings"
@@ -14,7 +15,20 @@ import (
 func Transcode(filename string, ch chan bool) {
 	startTime := time.Now()
 
-	cmd := exec.Command("ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=width,height", "-of", "csv=p=0", "tmp/videos/"+filename)
+	// Ensure the /tmp/output directory exists
+    outputDir := "/tmp/output/"
+    if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
+        log.Printf("Error creating %s: %v\n", outputDir, err)
+        ch <- false
+        return
+    }
+
+	// Check contents of tmp/videos
+	checkCmd := exec.Command("ls", "-lah", "/tmp/videos/")
+	checkOutput, _ := checkCmd.CombinedOutput()
+	log.Printf("Contents of tmp/videos:\n%v", string(checkOutput))
+
+	cmd := exec.Command("ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=width,height", "-of", "csv=p=0", "/tmp/videos/"+filename)
 	// cmd := exec.Command("ffmpeg", "-i", "internal/videos/"+filename, "-v", "quiet", "-select_streams", "v:0", "-show_entries", "stream=width,height", "-of", "csv=p=0")
 	var out bytes.Buffer
 	var stderr bytes.Buffer
@@ -62,19 +76,9 @@ func Transcode(filename string, ch chan bool) {
 				scaleFilter = fmt.Sprintf("scale=%d:%d", newWidth, q)
 			}
 			// Here you would adjust the FFmpeg command parameters based on the desired quality
-			outputFilename := "output_"+filename+ quality + ".mp4"
+			outputFilename := "output_" + filename + quality + ".mp4"
 			
-			// Check contents of tmp/videos
-			checkCmd := exec.Command("ls", "-lah", "tmp/videos/")
-			checkOutput, checkErr := checkCmd.CombinedOutput()
-			if checkErr != nil {
-				log.Printf("Error checking tmp/videos: %v\n", checkErr)
-				log.Printf("ls Output:\n%v", string(checkOutput))
-				return
-			}
-			log.Printf("Contents of tmp/videos:\n%v", string(checkOutput))
-
-			cmd := exec.Command("ffmpeg", "-i", "tmp/videos/"+filename, "-vf", scaleFilter, "-c:v", "libx264", "-crf", "23", "-c:a", "aac", "-strict", "experimental", "tmp/output/"+outputFilename)
+			cmd := exec.Command("ffmpeg", "-i", "/tmp/videos/"+filename, "-vf", scaleFilter, "-c:v", "libx264", "-crf", "23", "-c:a", "aac", "-strict", "experimental", "/tmp/output/"+outputFilename)
 			fmt.Println("Command:", cmd)
 			output, err := cmd.CombinedOutput()
 			if err != nil {
