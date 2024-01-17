e56e34c51f5f9062d43cb84b9431cc7fe5e700a8
Ayush Kaithwas <ayush.91011@gmail.com>
2024-01-18 01:09:27 +0530

    added http://localhost:5173 for testing due to cors

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -16,8 +16,7 @@ func (s *Server) RegisterRoutes() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(cors.Handler(cors.Options{
-		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedOrigins:   []string{"https://*", "http://*", "http://localhost:5173"}, // Use this to allow specific origin hosts
 		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
